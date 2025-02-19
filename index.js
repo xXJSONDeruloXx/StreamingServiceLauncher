275a54015c3e45de9d5b7933a08e62945685e59f
Aarron Lee <aarron-lee@users.noreply.github.com>
2025-02-19 14:45:45 -0500

    add --appname flag support

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const streamingServices = require("./services.json");
 const { app, BrowserWindow } = require("electron");
 
+const getServiceName = () => {
+  const flag = process.argv[process.argv.length - 1];
+  const appname = flag?.match(/--appname=([a-zA-Z0-9]+)/);
+  const extractedAppname = appname ? appname[1] : undefined;
+
+  // default is youtube
+  const serviceName = extractedAppname || process.env?.APP_NAME || "youtube";
+
+  return serviceName;
+};
+
 function createWindow() {
   const win = new BrowserWindow({
     fullscreen: true,
@@ -13,8 +24,7 @@ function createWindow() {
   // pressing alt can bring up the menu bar even when its hidden. This accounts for that and disables it entirely
   win.setMenu(null);
 
-  // default is youtube
-  const serviceName = process.env.APP_NAME || "youtube";
+  const serviceName = getServiceName();
 
   const { appUrl, userAgent, zoomFactor } =
     streamingServices[serviceName] || {};
@@ -25,7 +35,7 @@ function createWindow() {
       ? {
           userAgent,
         }
-      : {}
+      : {},
   );
 
   if (zoomFactor && zoomFactor > 0) {
